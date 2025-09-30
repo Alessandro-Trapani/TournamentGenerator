f87bc3fdbd743441e6796961acea610b839036d9
Alessandro <alessandro.trapani03@gmail.com>
2025-09-30 11:44:50 +0200

    init

---
+++ TorunamentGenerator/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace TI_Net2025_DemoCleanAsp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+      
+    }
+}
