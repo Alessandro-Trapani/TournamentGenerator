6d187885357284f384b7f14ea79d619b9024bc6f
skoiyo <theolaval70@gmail.com>
2025-09-30 14:04:58 +0200

    test

--- TorunamentGenerator/Program.cs
+++ TorunamentGenerator/Program.cs
@@ -22,7 +22,6 @@ builder.Services.AddDistributedMemoryCache();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
