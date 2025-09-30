44e7acec177be830db5e655e27fb6ed36a67bff1
skoiyo <theolaval70@gmail.com>
2025-09-30 14:09:06 +0200

    test commit and pull request

--- TorunamentGenerator/Program.cs
+++ TorunamentGenerator/Program.cs
@@ -22,6 +22,7 @@ builder.Services.AddDistributedMemoryCache();
 
 var app = builder.Build();
 
+// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
