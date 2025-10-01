82e7704e97bf8e4999804e6b35960fb33e6f56e8
skoiyo <skoiyogaming@gmail.com>
2025-10-01 11:23:18 +0200

    matchs teams tournaments entites

--- TournamentGenerator.DL/Entities/Teams.cs
+++ TournamentGenerator.DL/Entities/Teams.cs
@@ -1,14 +1,10 @@
 namespace TI_Net2025_DemoCleanAsp.DL.Entities;
 
-public class Tournaments
+public class Teams
 {
-    
     public int Id { get; set; }
     
     public string Name { get; set; }
     
-    public string Description { get; set; }
-    
-    public int NbPlayers { get; set; }
-    
+    public List<Teams> Members { get; set; }
 }
