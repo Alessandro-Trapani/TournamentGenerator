8ee539869f79fd55eec7b767e1d6a6986065bdbf
Vincent <fraikin.vincent@live.be>
2025-10-02 09:05:20 +0200

    repository

---
+++ TournamentGenerator.DAL/Repositories/TournamentRepository.cs
@@ -0,0 +1,13 @@
+﻿namespace TI_Net2025_DemoCleanAsp.DAL.Repositories;
+
+public class TournamentRepository
+{
+    private readonly string _connectionString;
+
+    public TournamentRepository(string connectionString)
+    {
+        _connectionString = connectionString;
+    }
+    
+    public void Create()
+}
---
+++ TournamentGenerator.DAL/Repositories/UserRepository.cs
@@ -0,0 +1,42 @@
+﻿namespace TI_Net2025_DemoCleanAsp.DAL.Repositories;
+
+public class UserRepository
+{
+    private readonly string _connectionString;
+
+    public UserRepository(string connectionString)
+    {
+        _connectionString = connectionString;
+    }
+
+    public void Add(User user)
+    {
+        using (var connection = new NpgsqlConnection(_connectionString))
+        using (var command = new NpgsqlCommand())
+        {
+            command.Connection = connection;
+            command.CommandText = @"
+                INSERT INTO ""User"" (email, username, password, role)
+                VALUES (@email, @username, @password, @role)
+                RETURNING id";
+
+            command.Parameters.AddWithValue("email", user.Email ?? (object)DBNull.Value);
+            command.Parameters.AddWithValue("username", user.Username ?? (object)DBNull.Value);
+            command.Parameters.AddWithValue("password", user.Password ?? (object)DBNull.Value);
+            command.Parameters.AddWithValue("role", user.Role?.ToString() ?? (object)DBNull.Value);
+
+            connection.Open();
+            int newId = (int)command.ExecuteScalar();
+        }
+    }
+
+    public User? GetUserByEmail(string email)
+    {
+        
+    }
+
+    public bool ExistUserByEmail(string email)
+    {
+        
+    }
+}
